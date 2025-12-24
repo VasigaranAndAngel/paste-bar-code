c017e7257dc5388c3691b0c581103017fec11ac3
VasigaranAndAngel <72515046+VasigaranAndAngel@users.noreply.github.com>
2025-12-24 15:45:42 +0530

    local_capturer.py - now gives actual camera options

--- src/capture_api/local_capturer.py
+++ src/capture_api/local_capturer.py
@@ -1,56 +1,26 @@
+import logging
 import threading
+from collections import defaultdict
 from collections.abc import Callable
 from typing import override
 
 import cv2
 from cv2.typing import MatLike
+from cv2_enumerate_cameras import enumerate_cameras
+from cv2_enumerate_cameras.camera_info import CameraInfo
 
 from .capturer_abc import Capturer
 
-# region Source - https://stackoverflow.com/a
-# Posted by G M, modified by community. See post 'Timeline' for change history
-# Retrieved 2025-12-16, License - CC BY-SA 4.0
-
-
-def _list_ports() -> tuple[list[int], list[int], list[int]]:
-    """
-    Test the ports and returns a tuple with the available ports and the ones that are working.
-    """
-    non_working_ports: list[int] = []
-    dev_port: int = 0
-    working_ports: list[int] = []
-    available_ports: list[int] = []
-    while len(non_working_ports) < 6:
-        camera = cv2.VideoCapture(dev_port)
-        if not camera.isOpened():
-            non_working_ports.append(dev_port)
-            print("Port %s is not working." % dev_port)
-        else:
-            is_reading, _ = camera.read()
-            w = camera.get(3)
-            h = camera.get(4)
-            if is_reading:
-                print("Port %s is working and reads images (%s x %s)" % (dev_port, h, w))
-                working_ports.append(dev_port)
-            else:
-                print(
-                    "Port %s for camera ( %s x %s) is present but does not reads."
-                    % (dev_port, h, w)
-                )
-                available_ports.append(dev_port)
-        dev_port += 1
-    return available_ports, working_ports, non_working_ports
-
-
-# endregion
+logger = logging.getLogger(__name__)
 
 
 class LocalCapturer(Capturer):
-    _option_to_port_map: dict[str, int] | None = None
+    _option_to_cameras_map: dict[str, tuple[CameraInfo, ...]] | None = None
     name: str | None = "Local Camera"
 
     def __init__(self) -> None:
-        self._selected_port: int = 0
+        self._selected_cameras: list[CameraInfo] = []
+        self._selected_option: str | None = None
         self._callback: Callable[[MatLike], None] | None = None
         self._run_capturing: bool = False
         self._thread: threading.Thread | None = None
@@ -75,35 +45,55 @@ class LocalCapturer(Capturer):
     @staticmethod
     @override
     def available_options() -> list[str]:
-        if LocalCapturer._option_to_port_map is None:
+        if LocalCapturer._option_to_cameras_map is None:
             LocalCapturer._update_available_options()
 
-        if LocalCapturer._option_to_port_map is not None:
-            return list(LocalCapturer._option_to_port_map.keys())
+        if LocalCapturer._option_to_cameras_map is not None:
+            return list(LocalCapturer._option_to_cameras_map.keys())
         else:
             return []
 
     @override
     def set_option(self, option: str) -> None:
         if (
-            LocalCapturer._option_to_port_map is None
-            or option not in LocalCapturer._option_to_port_map.keys()
+            LocalCapturer._option_to_cameras_map is None
+            or option not in LocalCapturer._option_to_cameras_map.keys()
         ):
             print("error. unknown option")
             return
 
-        self._selected_port = LocalCapturer._option_to_port_map[option]
+        self._selected_option = option
+        self._selected_cameras = sorted(
+            LocalCapturer._option_to_cameras_map[option],
+            key=lambda x: 0 if x.backend == cv2.CAP_MSMF else 1,
+        )
         # restart capturing to change the camera port
         if self._run_capturing:
             self.stop_capturing()
             self.start_capturing()
 
     def _read_camera(self) -> None:
-        camera = cv2.VideoCapture(self._selected_port)
+        if not len(self._selected_cameras) > 0:
+            logger.warning(f"Aborting reading camera. because there are no cameras selected.")
+            return
 
+        selected_index = 0
+        while True:
+            try:
+                if not len(self._selected_cameras) >= selected_index - 1:
+                    logger.warning(
+                        f'Aborting reading camera. None of the cameras: "{self._selected_cameras}" worked.'
+                    )
+                    return
+                camera = cv2.VideoCapture(self._selected_cameras[selected_index].index)
+                break
+            except:  # TODO: specify exceptions
+                selected_index += 1
+
+        logger.info(f'Starting capturing. Selected cam: "{self._selected_cameras[selected_index]}"')
         while self._run_capturing:
             is_reading, img = camera.read()
-            if is_reading:
+            if is_reading:  # TODO: implement falling back to other cameras if not is_reading.
                 if self._callback is not None:
                     self._callback(img)
 
@@ -111,11 +101,12 @@ class LocalCapturer(Capturer):
 
     @staticmethod
     def _update_available_options() -> None:
-        if LocalCapturer._option_to_port_map is None:
-            LocalCapturer._option_to_port_map = {}
+        LocalCapturer._option_to_cameras_map = {}
 
-        # available_ports = _list_ports()[1]
-        available_ports = [1, 2]
-        for port in available_ports:
-            option = f"Port {port}"
-            LocalCapturer._option_to_port_map[option] = port - 1
+        _map: dict[str, list[CameraInfo]] = defaultdict(list)
+        for camera in enumerate_cameras():
+            option = camera.name
+            _map[option].append(camera)
+
+        for option, cameras in _map.items():
+            LocalCapturer._option_to_cameras_map[option] = tuple(cameras)
