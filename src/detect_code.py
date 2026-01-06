ee3d260224d8aa7082cffe4482682d1bfc8d83b0
VasigaranAndAngel <72515046+VasigaranAndAngel@users.noreply.github.com>
2026-01-06 09:01:27 +0530

    remove drawing code text on frames

--- src/detect_code.py
+++ src/detect_code.py
@@ -13,7 +13,7 @@ def detect_code(frame: MatLike) -> tuple[str, MatLike]:
         code: str = barcode.data.decode("utf-8")  # pyright: ignore[reportUnknownMemberType, reportUnknownVariableType]
         _ = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # pyright: ignore[reportUnknownArgumentType]
 
-        font = cv2.FONT_HERSHEY_DUPLEX
-        _ = cv2.putText(frame, code, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)  # pyright: ignore[reportUnknownArgumentType]
+        # font = cv2.FONT_HERSHEY_DUPLEX
+        # _ = cv2.putText(frame, code, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)  # pyright: ignore[reportUnknownArgumentType]
 
     return code, frame  # pyright: ignore[reportUnknownVariableType]
